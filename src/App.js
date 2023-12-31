ce1ccef823281e29bec843e0ca899127a20f790c
Doug Chuchro <doug@chuchro.net>
2023-12-31 11:01:55 +0900

    basic mapbox search working and cleaned up unused files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
+import { AddressAutofill } from '@mapbox/search-js-react';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="content">
+        <h1>Mapbox Address Autofill Demo</h1>
+      </div>
+      <div className="form">
+        <form>
+          <AddressAutofill accessToken={process.env.REACT_APP_MAPBOX_API_KEY}>
+            <input
+              name="address" placeholder="Address" type="text"
+              autoComplete="address-line1"
+            />
+          </AddressAutofill>
+          <input
+            name="unit" placeholder="Unit number" type="text"
+            autoComplete="address-line2"
+          />
+          <input
+            name="city" placeholder="City" type="text"
+            autoComplete="address-level2"
+          />
+          <input
+            name="state" placeholder="State" type="text"
+            autoComplete="address-level1"
+          />
+          <input
+            name="country" placeholder="Country" type="text"
+            autoComplete="country"
+          />
+          <input
+            name="postcode" placeholder="Postcode" type="text"
+            autoComplete="postal-code"
+          />
+        </form>
+      </div>
     </div>
   );
 }
